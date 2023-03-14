df62b6e9de9b07c7ba4c3956adbbe5be3e2aa3fa
Arya Sunildath <aryamakers@MASTUDENT-Setup-User-C17GM6AXQ6L4.local>
2023-03-14 16:21:35 +0000

    Created class stringbuilder with rspec

---
+++ spec/string_builder_spec.rb
@@ -0,0 +1,22 @@
+require 'string_builder'
+
+RSpec.describe StringBuilder do
+  it "initializes with an epty string" do 
+    result = StringBuilder.new
+    expect(result.output). to eq ""
+  end
+
+  it "concatenates strings" do
+    result = StringBuilder.new
+    result.add('hello')
+    expect(result.output). to eq 'hello'
+  end
+
+  it 'reports its own length' do
+    result =  StringBuilder.new
+    result.add('hello')
+    expect(result.size). to eq 5
+  end
+end
+
+  
